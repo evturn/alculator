66da7912748d17bb3ed1063eb69108683e93de2a
Evan Turner <evturn@gmail.com>
2015-01-03 22:18:34 -0500

    created public folder and html

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var app = express();
 
-
-
-
-
+app.use(express.static('public'));
 
 app.listen(3000, function() {
 	console.log('Server running on localhost:3000 \n');
