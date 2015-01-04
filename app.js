51268b8572c90f5bc1b56a03c7a2b8bc10b545dc
Evan Turner <evturn@gmail.com>
2015-01-03 22:26:00 -0500

    file for 'get' only created'

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 
 app.use(express.static('public'));
 
+var logger = require('./logger');
+
 app.listen(3000, function() {
 	console.log('Server running on localhost:3000 \n');
 });
