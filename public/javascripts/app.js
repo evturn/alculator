45046d0c7d1c3aa30e31b3b86e0ee10943a2f2ba
Evan Turner <evturn@gmail.com>
2015-01-06 14:03:12 -0500

    url and http required for parsing

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -67,7 +67,7 @@ var alculatorView = new AlculatorView();
 				metabolism 	 = data.lbs * rate;
 				subLevel 		 = metricOz /  metabolism;
 				soberingRate = 0.015 * data.hours;
-				bac 			   = subLevel - soberingRate;
+				bac 			   = (subLevel - soberingRate).toFixed(2);
 				console.log(bac);
 				var round = new Round({lbs: data.lbs, hours: data.hours, drinks: data.drinks, abv: data.abv, sex: data.sex, rate: rate, bac: bac});
 				var roundView = new RoundView({model: round});
