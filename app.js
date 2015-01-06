cdf7ba1cebb810390b30114ac18f29e7d6f8e033
Evan Turner <evturn@gmail.com>
2015-01-06 18:26:58 -0500

    request to beers working, need to parse

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ var bodyParser = require('body-parser');
 var parseUrlencoded = bodyParser.urlencoded({extended: false});
 var logger = require('./logger');
   app.use(logger);
+var request = require('request');
+var url = require('url');
 
 app.use(express.static('public'));
 
 app.get('/', function(require, response) {
 	response.render('index.html');
 });
+
+
+app.get("/beers", function(req, res) {
+    var searchURL = 'http://api.brewerydb.com/v2/search?q=' + req.query.search + '&key=' + process.env.BREWERY_DB_KEY;
+    request(searchURL, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var results = {
+                brews: JSON.parse(body).Search 
+            }
+            res.send(results);
+        };
+    });
+});
+
 
 
 var rounds = {}
@@ -25,10 +41,6 @@ app.get('/api', function( request, response) {
     response.send('API is running');
 });
 
-
-app.get('/beers', function(response, request) {
-	response.send('id: [BREWERY_DB_KEY]', request.query)
-})
 
 app.listen(3000, function() {
 	console.log('Server running on localhost:3000 \n');
