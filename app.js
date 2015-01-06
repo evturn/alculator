5ef940a28221ab768ff688bd89285ad50032b95a
Evan Turner <evturn@gmail.com>
2015-01-05 19:24:14 -0500

    logger file works

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
+var logger = require('./logger');
+  app.use(logger);
+  
 app.use(express.static('public'));
 
 app.get('/', function(require, response) {
 	response.render('index.html');
 });
+
+
+
 
 app.listen(3000, function() {
 	console.log('Server running on localhost:3000 \n');
--- logger.js
+++ logger.js
@@ -4,10 +4,14 @@ module.exports = function(request, response, next) {
 	var url = request.url;
 	var method = request.method;
 
-	response.on('finish', function() {
+	response.on('finish', function(){
 		var duration = +new Date() - start;
-		var message = method + ' to ' + url + '\ntook ' + duration + ' ms \n\n';
+		var message = method + ' to ' + url +
+		'\ntook ' + duration + ' ms \n\n';
 		stream.write(message);
+
 	});
+
+
 	next();
 }
