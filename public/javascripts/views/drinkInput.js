3ef24a2f307210f8df8d99b0004875392053190b
Evan Turner <evturn@gmail.com>
2015-01-07 18:10:49 -0500

    drinkInput view created

--- public/javascripts/views/drinkInput.js
+++ public/javascripts/views/drinkInput.js
@@ -1,4 +1,10 @@
 var DrinkInput = Backbone.View.extend({
 	el: $('#hidden-input'),
 	template: _.($('#drink-input-template').html()),
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.html(this.template());
+	}
 });
