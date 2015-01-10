a7bf8b52a484c8f5659603fe8490bb3249784492
Evan Turner <evturn@gmail.com>
2015-01-10 12:17:00 -0500

    moving the stages out of the array and into separate templates

--- public/javascripts/views/stage.js
+++ public/javascripts/views/stage.js
@@ -1,11 +1,11 @@
 var StageView = Backbone.View.extend({
 	el: $('#stage'),
-	template: _.template($('#stage-template').html()),
+	stageZero: _.template($('#stage--zero-template').html()),
 	initialize: function() {
 		this.render();
 	},
 	render: function() {
-		this.$el.html(this.template(this.models));
+		this.$el.html(this.template());
 		return this;
 	}
 });
