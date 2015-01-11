7099b838a7ead9a8e669056bc03eff16fa9dc27f
Evan Turner <evturn@gmail.com>
2015-01-10 22:09:54 -0500

    addOne function added to BoozeCollection

--- public/javascripts/views/boozeListView.js
+++ public/javascripts/views/boozeListView.js
@@ -1,10 +1,11 @@
 var BoozeListView = new Backbone.View.extend({
 	render: function() {
-		this.collection.forEach(function(boozeItem) {
+		this.collection.forEach(this.addOne, this);	
+	}
+	addOne: function(boozeItem) {
 			var boozeView = new BoozeView({model: boozeItem});
 			this.$el.append(boozeView.render().el);
-		});
-	}
+		}
 });
 
 console.log('BoozeListView');
