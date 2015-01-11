c29395475e65fcbb4d582f79a7ddb8fbba2aa907
Evan Turner <evturn@gmail.com>
2015-01-10 23:28:25 -0500

    tagName, id, className added for BoozeItemView

--- public/javascripts/views/boozeItemView.js
+++ public/javascripts/views/boozeItemView.js
@@ -1,4 +1,7 @@
 var BoozeItemView = Backbone.View.extend({
+	tagName: 'div',
+	id: 'booze-view',
+	className: 'booze'
 	render: function() {
 		var html = '<h4>' + this.model.get('name') + '</h4>'
 							 '<h4>' + this.model.get('abv') + '</h4>'
