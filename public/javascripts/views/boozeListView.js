84700ce4a220fd5b688dd084b73d703c8f52d0c3
Evan Turner <evturn@gmail.com>
2015-01-10 22:20:32 -0500

    added hideModel function on collection

--- public/javascripts/views/boozeListView.js
+++ public/javascripts/views/boozeListView.js
@@ -1,14 +1,18 @@
 var BoozeListView = new Backbone.View.extend({
 	initialize: function() {
 		this.collection.on('add', this.addOne, this);
-	}
-	render: function() {
-		this.collection.forEach(this.addOne, this);	
-	}
+		this.collection.on('reset', this.addAll, this);
+	},
 	addOne: function(boozeItem) {
 			var boozeView = new BoozeView({model: boozeItem});
 			this.$el.append(boozeView.render().el);
-		}
+	},
+	allAll: function() {
+		this.collection.forEach(this.addOne, this);	
+	},
+	render: function() {
+		this.addAll();
+	}
 });
 
 console.log('BoozeListView');
