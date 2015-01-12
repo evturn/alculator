431d5fdf73525233b6ecc13e81c1f218faad2354
Evan Turner <evturn@gmail.com>
2015-01-12 14:06:22 -0500

    add function for booze view to add drinks when click on a beverage

--- public/javascripts/views/beerItemView.js
+++ public/javascripts/views/beerItemView.js
@@ -1,11 +1,14 @@
 var BeerItemView = Backbone.View.extend({
 	el: '#beverage-items',
 	template: _.template($('#beer-item-view-template').html()),
+	events: {
+
+	},
 	render: function() {
 		var beerAttributes = this.model.toJSON();
 		this.$el.append(this.template(beerAttributes));
 		return this;
-	}
+	},
 });
 
 console.log('BeerItemView');
--- public/javascripts/views/boozeItemView.js
+++ public/javascripts/views/boozeItemView.js
@@ -1,11 +1,29 @@
 var BoozeItemView = Backbone.View.extend({
 	el: '#booze-items',
 	template: _.template($('#booze-item-view-template').html()),
+	events: {
+		'click .add-booze': 'addBooze'
+	},
 	render: function() {
 		var boozeAttributes = this.model.toJSON();
 		this.$el.append(this.template(boozeAttributes));
 		return this;
-	}
+	},
+	addBooze:function(e){
+    e.preventDefault();
+    var name = this.model.find("name").val();
+    var abv = this.model.find("abv").val();
+    var img = this.model.find("img").val();
+    var ounces = this.model.find("ounces").val();
+    var newBooze = new BoozeItem({
+    	name: name,
+    	abv: abv,
+    	img: img,
+    	ounces: ounces,
+    	selected, true
+    	});  
+    this.collection.add(newBooze);  
+  },
 });
 
 console.log('BoozeItemView');
