9031b0ca3be15104e8d31c20055da3b4eda9838f
Evan Turner <evturn@gmail.com>
2015-01-12 10:24:02 -0500

    Liquor Item View added

--- public/javascripts/views/liquorItemView.js
+++ public/javascripts/views/liquorItemView.js
@@ -1,5 +1,11 @@
 var LiquorItemView = Backbone.View.extend({
-	
+		el: '#liquor-items',
+	template: _.template($('#liquor-item-view-template').html()),
+	render: function() {
+		var liquorAttributes = this.model.toJSON();
+		this.$el.append(this.template(liquorAttributes));
+		return this;
+	}
 });
 
 console.log('LiquorViewItem');
