c29db844b60558e944af87b0f7ad5116f6425663
Evan Turner <evturn@gmail.com>
2015-01-11 19:33:44 -0500

    BeerItemView appending to DOM

--- public/javascripts/views/beerItemView.js
+++ public/javascripts/views/beerItemView.js
@@ -1,14 +1,18 @@
 var BeerItemView = Backbone.View.extend({
-	el: '#booze-content',
+	el: '#beer-items',
 	template: _.template($('#beer-item-view-template').html()),
 	initialize: function() {
 		this.render();
 	},
 	render: function() {
 		var beerAttributes = this.model.toJSON();
-		this.$el.html(this.template(beerAttributes));
+		this.$el.append(this.template(beerAttributes));
 		return this;
 	}
 });
 
 console.log('BeerItemView');
+
+
+var beerItem = new BeerItem();
+var beerItemView = new BeerItemView({model: beerItem})
