56ad9729d3f81f523f749d53353b85483fde7622
Evan Turner <evturn@gmail.com>
2015-01-11 20:15:02 -0500

    No more duplicates on page, beer models loading

--- public/javascripts/views/beerItemView.js
+++ public/javascripts/views/beerItemView.js
@@ -1,9 +1,6 @@
 var BeerItemView = Backbone.View.extend({
 	el: '#beer-items',
 	template: _.template($('#beer-item-view-template').html()),
-	initialize: function() {
-		this.render();
-	},
 	render: function() {
 		var beerAttributes = this.model.toJSON();
 		this.$el.append(this.template(beerAttributes));
