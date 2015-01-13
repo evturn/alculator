f4d9dc8a9fec09d5a9bfe6923cd842a7bdffe108
Evan Turner <evturn@gmail.com>
2015-01-12 21:14:53 -0500

    remove button coming up on hover

--- public/javascripts/views/boozeItemView.js
+++ public/javascripts/views/boozeItemView.js
@@ -1,9 +1,9 @@
 var BoozeItemView = Backbone.View.extend({
-	className: 'col-xs-4 align drink-space booze-item',
+	className: 'col-xs-12 align drink-space booze-item',
 	template: _.template($('#booze-item-view-template').html()),
 	render: function() {
-		var boozeAttributes = this.model.toJSON();
-		this.$el.append(this.template(boozeAttributes));
+		var newBooze = this.model.toJSON();
+		this.$el.append(this.template(newBooze));
 		return this;
 	},
 });
