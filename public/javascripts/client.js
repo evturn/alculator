b5921d37991e1e6827dc5d05071653e91e126b99
Evan Turner <evturn@gmail.com>
2015-01-13 15:47:44 -0500

    tabs add and remove the class of 'active' when clicked

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -15,18 +15,25 @@ boozeQueueView = new BoozeQueueView();
 
 
 $('#liquor-tab').on('click', function() {
+	$(this).parent().find('li.active').removeClass('active');
+  $(this).addClass('active');
 	var liquorItems = new LiquorItems();
 	liquorItems.fetch({reset: true});
 	liquorItemView = new LiquorItemsView({collection: liquorItems});
 });
 
 $('#wine-tab').on('click', function() {
+  $(this).parent().find('li.active').removeClass('active');
+  $(this).addClass('active');
 	var wineItems = new WineItems();
 	wineItems.fetch({reset: true});
 	wineItemView = new WineItemsView({collection: wineItems});
+	console.log(this);
 });
 
 $('#beer-tab').on('click', function() {
+	$(this).parent().find('li.active').removeClass('active');
+  $(this).addClass('active');
 	var beerItems = new BeerItems();
 	beerItems.fetch({reset: true});
 	beerItemView = new BeerItemsView({collection: beerItems});
