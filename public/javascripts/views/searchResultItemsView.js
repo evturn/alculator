e57b956d29635aa084ab8fbead64eb37b09f8bba
Evan Turner <evturn@gmail.com>
2015-01-16 11:44:06 -0500

    search result items view added

--- public/javascripts/views/searchResultItemsView.js
+++ public/javascripts/views/searchResultItemsView.js
@@ -1,4 +1,10 @@
 var SearchResultItemsView = Backbone.View.extend({
-
-	
+	el: '#search-results',
+	initialize: function() {
+		this.listenTo(this.collection, 'reset', this.addOne);
+	},
+	addOne: function(model) {
+		var view = new SearchResultItem({model: model});
+		this.$el.append(view.el);
+	},
 });
