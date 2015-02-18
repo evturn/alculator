9e58ca72f438f3bc58a1394660cd3fb29e5a6c38
Evan Turner <evturn@gmail.com>
2015-02-18 03:34:45 -0500

    client file cleaned up and moved into a backbone view

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,43 +1,37 @@
 new NavView();
-var beerItems = new BeerItems();
-beerItems.fetch({reset: true});
-new BeerItemsView({collection: beerItems});
-var wineItems = new WineItems();
-wineItems.fetch({reset: true});
-new WineItemsView({collection: wineItems});
-var stages = new Stages({reset: true});
-stages.fetch();
-var boozeItems = new BoozeItems({reset: true, merge: false});
-new UserInputView();
-new BoozeItemsView();
+
+
+
+
+
 
 $(function() {
 	boozeQueueView = new BoozeQueueView();
 
-	$('#liquor-tab').on('click', function() {
-		$(this).parent().find('li.active').removeClass('active');
-	  $(this).addClass('active');
-	  $('#search-query').hide();
-		var liquorItems = new LiquorItems();
-		liquorItems.fetch({reset: true});
-		liquorItemView = new LiquorItemsView({collection: liquorItems});
-	});
+// 	$('#liquor-tab').on('click', function() {
+// 		$(this).parent().find('li.active').removeClass('active');
+// 	  $(this).addClass('active');
+// 	  $('#search-query').hide();
+// 		var liquorItems = new LiquorItems();
+// 		liquorItems.fetch({reset: true});
+// 		liquorItemView = new LiquorItemsView({collection: liquorItems});
+// 	});
 
-	$('#beer-tab').on('click', function() {
-		$(this).parent().find('li.active').removeClass('active');
-	  $(this).addClass('active');
-	  new QueryView();
-		var beerItems = new BeerItems();
-		beerItems.fetch({reset: true});
-		beerItemView = new BeerItemsView({collection: beerItems});
-	});
+// 	$('#beer-tab').on('click', function() {
+// 		$(this).parent().find('li.active').removeClass('active');
+// 	  $(this).addClass('active');
+// 	  new QueryView();
+// 		var beerItems = new BeerItems();
+// 		beerItems.fetch({reset: true});
+// 		beerItemView = new BeerItemsView({collection: beerItems});
+// 	});
 
-	$('#wine-tab').on('click', function() {
-	  $(this).parent().find('li.active').removeClass('active');
-	  $(this).addClass('active');
-	  $('#search-query').hide();
-		var wineItems = new WineItems();
-		wineItems.fetch({reset: true});
-		wineItemView = new WineItemsView({collection: wineItems});
-	});
+// 	$('#wine-tab').on('click', function() {
+// 	  $(this).parent().find('li.active').removeClass('active');
+// 	  $(this).addClass('active');
+// 	  $('#search-query').hide();
+// 		var wineItems = new WineItems();
+// 		wineItems.fetch({reset: true});
+// 		wineItemView = new WineItemsView({collection: wineItems});
+// 	});
 });
--- public/javascripts/views/NavView.js
+++ public/javascripts/views/NavView.js
@@ -2,10 +2,47 @@ var NavView = Backbone.View.extend({
 	el: '#nav',
 	navTemplate: _.template($('#nav-template').html()),
 	initialize: function() {
+		this.instantiateCollections();
+		this.fetchCollections();
 		this.render();
+		this.renderApp();
+	},
+	events: {
+		'click #beer-tab'  : 'setBeerItems',
+		'click #liquor-tab': 'setLiquorItems',
+		'click #wine-tab'	 : 'setWineItems'
+	},
+	renderApp: function() {
+		this.setWineItems();
+		new UserInputView();
+		new BoozeItemsView();
+		boozeQueueView = new BoozeQueueView();
 	},
 	render: function() {
 		this.$el.html(this.navTemplate());
 		return this;
+	},
+	setBeerItems: function() {
+		new BeerItemsView({collection: beerItems});
+		new QueryView();
+	},
+	setWineItems: function() {
+		new WineItemsView({collection: wineItems});
+	},
+	setLiquorItems: function() {
+		new LiquorItemsView({collection: liquorItems});
+	},
+	instantiateCollections: function() {
+		boozeItems = new BoozeItems({reset: true, merge: false});
+		beerItems = new BeerItems();
+		wineItems = new WineItems();
+		liquorItems = new LiquorItems();
+		stages = new Stages({reset: true});
+	},
+	fetchCollections: function() {
+		stages.fetch();
+		beerItems.fetch({reset: true});
+		wineItems.fetch({reset: true});
+		liquorItems.fetch({reset: true});
 	},
 });
