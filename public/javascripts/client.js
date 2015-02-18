82ff0e9ddbe3ee0ca66bbadd111d2f601b82b711
Evan Turner <evturn@gmail.com>
2015-02-18 17:34:00 -0500

    sponsored by blockbuster

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,3 +1,27 @@
 new AppView();
 
 ENTER_KEY = 13;
+
+$(function() {
+
+	$(document).load().scrollTop(0);
+
+  $('#full-cover').on({
+    'mousewheel' : function(e) {
+        if (e.target.id == 'el') return;
+        e.preventDefault();
+    }
+  });
+
+  $('#full-cover').on('click', function(e) {
+		e.preventDefault();
+			setTimeout(function(){ 
+				$('#full-cover').remove();
+				$(window).scrollTop(0);
+			}, 1500);
+	});
+  
+  	
+
+});
+
