fb2c400bf9ab97b04af396b6af180070a5aff63c
Evan Turner <evturn@gmail.com>
2015-03-25 02:06:40 -0400

    single and list wine views removed

--- public/javascripts/collections/wine.js
+++ public/javascripts/collections/wine.js
@@ -2,6 +2,3 @@ var Wine = Backbone.Collection.extend({
 	model: Beverage,
 	url: '/wine'
 });
-
-var wine = new Wine();
-wine.fetch();
--- public/javascripts/models/wineItem.js
+++
@@ -1,9 +0,0 @@
-var WineItem = Backbone.Model.extend({
-	defaults: {
-		name: 'Uh oh',
-		abv: 'none',
-		img: 'images/alc.png',
-		ounces: 'none'
-	}
-});
-
