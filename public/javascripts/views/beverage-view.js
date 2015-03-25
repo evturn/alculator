7b75d6c3ad3b783ccdf88253a11a792d9f1bae9e
Evan Turner <evturn@gmail.com>
2015-03-25 04:31:02 -0400

    beverage item models initializing user selections

--- public/javascripts/views/beverage-view.js
+++ public/javascripts/views/beverage-view.js
@@ -6,22 +6,21 @@ var BeverageView = Backbone.View.extend({
 		this.render();
 	},
 	events: {
-		'click img': 'acknowledge'
+		'click .beverage-image': 'select'
 	},
   render: function(){
     this.$el.html(this.template(this.model.toJSON()));
     return this;
   },
-	select: function() {
+	renderSelection: function() {
 		var userTab  = new Tab({reset: true, merge: false});
 		var selection = new Beverage(this.model.attributes);
 		selection.set({selected: true});
 		userTab.push(selection);
 		var tabItemView = new TabItemView({model: selection});
 	},
-	 acknowledge: function(e) {
-	 	this.select();
-    console.log('i felt that!', $(e.currentTarget));
+	select: function(e) {
+	 	this.renderSelection();
     $(e.currentTarget).addClass('screen-click');
     setTimeout(function() {
       $('img').removeClass('screen-click');
