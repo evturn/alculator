44956bde7029c912787cf1da549d0c9b4f057f49
Evan Turner <evturn@gmail.com>
2015-03-26 17:53:58 -0400

    function created to make array of drinks

--- public/javascripts/views/user-input.js
+++ public/javascripts/views/user-input.js
@@ -22,10 +22,12 @@ var UserInput = Backbone.View.extend({
 		var drinks = userTab.length;
 		var abvs  = [];
 		var ozs 	= [];
+		
 		function getSum(array) {
 			var sum = array.reduce(function (a, b) {return a + b});
 			return sum;
 		}
+	 	
 	 	function bac(abv, oz, lbs, hours, rate) {
 	 		var abvAverage 	 = abv / drinks;
 	 		var ethanolOz 	 = oz * (abvAverage * 0.01);	
@@ -35,21 +37,25 @@ var UserInput = Backbone.View.extend({
 			var soberingRate = 0.015 * hours;
 			return (subTotal - soberingRate).toFixed(2);
 	 	}
-		userTab.each(function(model) {
-			var abv 	 = model.get('abv');
-			var ounces = model.get('ounces');
 
-			abvs.push(abv);
-			ozs.push(ounces);
-		});
+	 	function createList(collection){
+			collection.each(function(model) {
+				var abv 	 = model.get('abv');
+				var ounces = model.get('ounces');
+
+				abvs.push(abv);
+				ozs.push(ounces);
+			});		
+	 	}
+
+	 	createList(userTab);
 		var abvSum = getSum(abvs);
 		var ozSum = getSum(ozs);
-
-		
+	
 	 	var lbs		= parseInt($('#lbs').val());
 		var hours = parseInt($('#hours').val());
-		var sex 	= $('#male').val();
-	 	var rate  = sex === 'male' ? 0.73 : 0.66;
+	 	var rate  = $('#male').val() === 'male' ? 0.73 : 0.66;
+	 	console.log(rate);
 
 	 	var results = bac(abvSum, ozSum, lbs, hours, rate);
 
