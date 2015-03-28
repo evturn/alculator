1bb3ff0aeab23d08639b77471223301a908e7c7c
Evan Turner <evturn@gmail.com>
2015-03-27 22:23:30 -0400

    description on duration for user-input revised

--- server.js
+++ server.js
@@ -52,26 +52,26 @@ app.get('/queries', function(request, response) {
 	response.status(200).json();
 });
 
-allBeers  = [{name: "Light", abv: 4, img: "images/bottle.png", ounces: 12},
+var allBeers  = [{name: "Light", abv: 4, img: "images/bottle.png", ounces: 12},
 		{name: "Standard", abv: 5, img: "images/bottle.png", ounces: 12},
 		{name: "Strong", abv: 7, "img": "images/bottle.png", ounces: 12},
 		{name: "Stronger", abv: 10, "img": "images/bottle.png", ounces: 12}];
 
-allLiquor = [{name: "shot", abv: 40, img: "images/shot.png", ounces: 1.5},
+var allLiquor = [{name: "shot", abv: 40, img: "images/shot.png", ounces: 1.5},
 		{name: "Lowball", abv: 40, img: "images/low.png", ounces: 7},
 		{name: "Highball", abv: 40, img: "images/high.png", ounces: 8},
 		{name: "Cocktail", abv: 45, img: "images/cocktail.png", ounces: 5},
 		{name: "Margarita", abv: 25, img: "images/margarita.png", ounces: 9},
 		{name: "Cordial", abv: 20, img: "images/cordial.png", ounces: 2}];
 
-allWine		= [{name: "Red", abv: 13, img: "images/redglass.png", ounces: 5},
+var allWine		= [{name: "Red", abv: 13, img: "images/redglass.png", ounces: 5},
 		{name: "White", abv: 12.5, img: "images/whiteglass.png", ounces: 5},
 		{name: "Champagne", abv: 12, img: "images/champagneglass.png", ounces: 5.5},
 		{name: "Red", abv: 13, img: "images/bottle-red.png", ounces: 25},
 		{name: "White", abv: 12.5, img: "images/bottle-white.png", ounces: 25},
 		{name: "Champagne", abv: 12, img: "images/bottle-champagne.png", ounces: 25}]
 
-outcomes = [
+var outcomes = [
 	{name: "stage-zero", description: "No significant trace of alcohol in your blood", color: "zero", id: 0},
 	{name: "stage-one", description: "No loss of coordination, slight euphoria and loss of shyness. Mildly relaxed and maybe a little lightheaded", color: "one", id: 1},
 	{name: "stage-two", description: "Some minor impairment of reasoning and memory, lowering of caution. Your behavior may become exaggerated and emotions intensified (Good emotions are better, bad emotions are worse)", color: "one", id: 2},
