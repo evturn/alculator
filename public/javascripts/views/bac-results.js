7b067805447daff6d7e4a89a4a1c5872f188fbf3
Evan Turner <evturn@gmail.com>
2015-03-27 21:09:34 -0400

    outcomes given id to replace 'stage'

--- public/javascripts/views/bac-results.js
+++ public/javascripts/views/bac-results.js
@@ -2,10 +2,51 @@ var BacResults = Backbone.View.extend({
 	el: '#results',
 	resultsTemplate: _.template($('#results-template').html()),
 	initialize: function() {
-		this.render();
+		outcomes = new Outcomes({reset: true});
+		outcomes.fetch();
+		this.diagnosis();
 	},
 	render: function() {
 		this.$el.html(this.resultsTemplate(this.model.toJSON()));
 		return this;
 	},
+	diagnosis: function(){
+		var bac = this.model.get('bac');
+		if (bac < 0.02) {
+					
+			} else if (bac < 0.04) {
+				
+				
+			} else if (bac < 0.06) {
+				
+				
+			} else if (bac < 0.07) {
+				
+				
+			} else if (bac < 0.10) {
+				
+				
+			} else if (bac < 0.13) {
+				
+				
+			} else if (bac < 0.16) {
+				
+				
+			} else if (bac < 0.20) {
+				
+				
+			} else if (bac < 0.25) {
+				
+				
+			} else if (bac < 0.30) {
+				
+				
+			} else if (bac >= 0.30) {
+				
+				
+			} else {
+				alert('You sure you entered all your info?');
+			}
+	},
+	}
 });
--- server.js
+++ server.js
@@ -72,17 +72,17 @@ allWine		= [{name: "Red", abv: 13, img: "images/redglass.png", ounces: 5},
 		{name: "Champagne", abv: 12, img: "images/bottle-champagne.png", ounces: 25}]
 
 outcomes = [
-	{name: "stage-zero", description: "No significant trace of alcohol in your blood", color: "zero", stage: 0},
-	{name: "stage-one", description: "No loss of coordination, slight euphoria and loss of shyness. Mildly relaxed and maybe a little lightheaded", color: "one", stage: 1},
-	{name: "stage-two", description: "Some minor impairment of reasoning and memory, lowering of caution. Your behavior may become exaggerated and emotions intensified (Good emotions are better, bad emotions are worse)", color: "one", stage: 2},
-	{name: "stage-three", description: "Slight impairment of balance, speech, vision, reaction time, and hearing. Judgment and self-control are reduced, and caution, reason and memory are impaired, .08 is legally impaired and it is illegal to drive at this level. You probably believe that you are functioning better than you really are", color: "two", stage: 3},
-	{name: "stage-four", description: "Significant impairment of motor coordination and loss of good judgment. Speech may be slurred; balance, vision, reaction time and hearing will be impaired", color: "two", stage: 4},
-	{name: "stage-five", description: "Significant impairment of motor coordination and loss of good judgment. Speech may be slurred; balance, vision, reaction time and hearing will be impaired", color: "three", stage: 5},
-	{name: "stage-six", description: "Gross motor impairment and lack of physical control. Blurred vision and major loss of balance. Euphoria is reduced and dysphoria (anxiety, restlessness) is beginning to appear. Judgment and perception are severely impaired", color: "three", stage: 6},
-	{name: "stage-seven", description: "Dysphoria predominates, nausea may appear. The drinker has the appearance of a 'sloppy drunk'", color: "four", stage: 7},
-	{name: "stage-eight", description: "Feeling dazed, confused or otherwise disoriented. May need help to stand or walk. If you injure yourself you may not feel the pain. Some people experience nausea and vomiting at this level. The gag reflex is impaired and you can choke if you do vomit. Blackouts are likely at this level so you may not remember what has happened", color: "four", stage: 8},
-	{name: "stage-nine", description: "You have little comprehension of where you are. You may pass out suddenly and be difficult to awaken. Coma is possible. This is the level of surgical anesthesia", color: "five", stage: 9},
-	{name: "stage-ten", description: "Onset of coma, probable death due to respitory arrest", color: "five", stage: 10}]
+	{name: "stage-zero", description: "No significant trace of alcohol in your blood", color: "zero", id: 0},
+	{name: "stage-one", description: "No loss of coordination, slight euphoria and loss of shyness. Mildly relaxed and maybe a little lightheaded", color: "one", id: 1},
+	{name: "stage-two", description: "Some minor impairment of reasoning and memory, lowering of caution. Your behavior may become exaggerated and emotions intensified (Good emotions are better, bad emotions are worse)", color: "one", id: 2},
+	{name: "stage-three", description: "Slight impairment of balance, speech, vision, reaction time, and hearing. Judgment and self-control are reduced, and caution, reason and memory are impaired, .08 is legally impaired and it is illegal to drive at this level. You probably believe that you are functioning better than you really are", color: "two", id: 3},
+	{name: "stage-four", description: "Significant impairment of motor coordination and loss of good judgment. Speech may be slurred; balance, vision, reaction time and hearing will be impaired", color: "two", id: 4},
+	{name: "stage-five", description: "Significant impairment of motor coordination and loss of good judgment. Speech may be slurred; balance, vision, reaction time and hearing will be impaired", color: "three", id: 5},
+	{name: "stage-six", description: "Gross motor impairment and lack of physical control. Blurred vision and major loss of balance. Euphoria is reduced and dysphoria (anxiety, restlessness) is beginning to appear. Judgment and perception are severely impaired", color: "three", id: 6},
+	{name: "stage-seven", description: "Dysphoria predominates, nausea may appear. The drinker has the appearance of a 'sloppy drunk'", color: "four", id: 7},
+	{name: "stage-eight", description: "Feeling dazed, confused or otherwise disoriented. May need help to stand or walk. If you injure yourself you may not feel the pain. Some people experience nausea and vomiting at this level. The gag reflex is impaired and you can choke if you do vomit. Blackouts are likely at this level so you may not remember what has happened", color: "four", id: 8},
+	{name: "stage-nine", description: "You have little comprehension of where you are. You may pass out suddenly and be difficult to awaken. Coma is possible. This is the level of surgical anesthesia", color: "five", id: 9},
+	{name: "stage-ten", description: "Onset of coma, probable death due to respitory arrest", color: "five", id: 10}]
 
 var beerChoice = function(responseObject) {
 	var	beersList = [];
