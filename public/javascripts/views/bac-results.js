5b60bb221c9ded38a6041cfc2f450198efcff0b8
Evan Turner <evturn@gmail.com>
2015-03-27 20:01:30 -0400

    drinker object collecting all stats on page

--- public/javascripts/views/bac-results.js
+++ public/javascripts/views/bac-results.js
@@ -1,3 +1,11 @@
 var BacResults = Backbone.View.extend({
-	el: '#results'
+	el: '#results',
+	resultsTemplate: _.template($('#results-template').html()),
+	initialize: function() {
+		this.render();
+	},
+	render: function() {
+		this.$el.html(this.resultsTemplate(this.model.toJSON()));
+		return this;
+	},
 });
--- public/javascripts/views/user-input.js
+++ public/javascripts/views/user-input.js
@@ -19,16 +19,16 @@ var UserInput = Backbone.View.extend({
 	},
 	calculate: function(e) {
 		e.preventDefault();
-		var user 			 = new Drinker();
-		var collection = userTab;
-		var drinks 		 = collection.length;
 		var abvs  		 = [];
 		var ozs 	     = [];
 		
 		function Drinker() {
-	 		this.lbs	 = parseInt($('#lbs').val());
-			this.hours = parseInt($('#hours').val());
-	 		this.rate  = $('#male').val() === 'male' ? 0.73 : 0.66;
+	 		this.lbs	  = parseInt($('#lbs').val());
+			this.hours  = parseInt($('#hours').val());
+	 		this.rate   = $('#male').val() === 'male' ? 0.73 : 0.66;
+		  this.drinks = userTab.length;
+	 		this.abv 		= getSum(abvs);
+	 		this.ounces = getSum(ozs);
 		}
 
 		function getSum(array) {
@@ -36,31 +36,36 @@ var UserInput = Backbone.View.extend({
 			return sum;
 		}
 	 	
-	 	function bac(abv, oz, drinker) {
-	 		var abvAverage 	 = abv / drinks;
-	 		var ethanolOz 	 = oz * (abvAverage * 0.01);	
-	 		var metricOz	   = (ethanolOz * 5.14).toFixed(2);
-			var metabolism 	 = user.lbs * user.rate;
-			var subTotal 		 = (metricOz /  metabolism).toFixed(2);
-			var soberingRate = 0.015 * user.hours;
-			return (subTotal - soberingRate).toFixed(2);
-	 	}
-
-	 	function createList(collection){
+	 	function newDrinker(collection){
 			collection.each(function(model) {
 				var abv 	 = model.get('abv');
 				var ounces = model.get('ounces');
 
 				abvs.push(abv);
 				ozs.push(ounces);
-			});		
+			});	
+
+			var user = new Drinker();
+	 		
 	 	}
 
-	 	createList(userTab);
+		var user = newDrinker(userTab);
 
-		var abvSum 	= getSum(abvs);
-		var ozSum  	= getSum(ozs);
-	 	var results = bac(abvSum, ozSum, user);
+	 	function bac(drinker) {
+
+
+
+	 		var abvAverage 	 = drinker.abv / drinker.drinks;
+	 		var ethanolOz 	 = drinker.oz * (abvAverage * 0.01);	
+	 		var metricOz	   = (ethanolOz * 5.14).toFixed(2);
+			var metabolism 	 = drinker.lbs * drinker.rate;
+			var subTotal 		 = (metricOz /  metabolism).toFixed(2);
+			var soberingRate = 0.015 * drinker.hours;
+			return (subTotal - soberingRate).toFixed(2);
+	 	}
+
+
+	 	var results = bac(user);
 
 		var product 		 = new Result({bac: results});
 		var resultsModel = new Results({model: product});
