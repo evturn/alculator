94adb12a948b5601a3102921554d7a310f57837e
Evan Turner <evturn@gmail.com>
2015-05-05 14:23:44 -0400

    new switch case statement handling beverages at bar

--- public/javascripts/views/navbar.js
+++ public/javascripts/views/navbar.js
@@ -1,12 +1,12 @@
 var Navbar = Backbone.View.extend({
-	el: '#nav',
+	el: '.container-nav',
 	navTemplate: _.template($('#nav-template').html()),
 	initialize: function() {
 		this.init();
 		this.render();
 	},
 	events: {
-		'click #beverage-nav' : 'active'	
+		'click .nav-bar-section' : 'active'	
 	},
 	render: function() {
 		this.$el.html(this.navTemplate());
@@ -14,22 +14,23 @@ var Navbar = Backbone.View.extend({
 	},
 	active: function(e) {
 		e.preventDefault();
-		$('#beverage-nav').find('li.active').removeClass('active');
-		$(e.target).closest('li').addClass('active');
-		var id = ($('.active').attr('id'));
+		var target = $(e.currentTarget);
+		var button = target.find('button');
+		var id = button.data('id');
+		target.find('button.active').removeClass('active');
+		button.addClass('active');
 		this.alternate(id);
 	},
 	alternate: function(id) {
-		if (id === 'beer-tab') {
-			collection = new Beers();	
-		} else if (id === 'wine-tab') {
-			collection = new Wine();
-		} else if (id === 'liquor-tab') {
-			collection = new Liquor();
-		} else {
-			collection = new Wine();
+		var id = id || 2;
+		switch (true) {
+			case (id === 1): collection = new Liquor();	
+			break
+			case (id === 2): collection = new Wine();	
+			break
+			case (id === 3): collection = new Beers();	
+			break
 		}
-
 		collection.fetch({reset: true});
 		var beveragesList = new BeveragesListView({collection: collection});
 	},
