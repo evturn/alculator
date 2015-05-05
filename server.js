876f7d0ade7684839d49d3746e8461c102eb4f42
Evan Turner <evturn@gmail.com>
2015-05-05 13:08:38 -0400

    ejs templating replacing index.html

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var express 				= require('express');
 var app 						= express();
 var bodyParser 			= require('body-parser');
+var ejs 						= require('ejs');
 var parseUrlencoded = bodyParser.urlencoded({extended: false});
 var logger 					= require('./logger');
 app.use(logger);
 var request 				= require('request');
 
+
 app.use(express.static('public'));
 
+app.set('views', __dirname + '/public/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function(require, response) {
-	response.render('index.html');
+	response.render('layout');
 });
 
 app.get('/beers', function(request, response) {
@@ -36,16 +41,6 @@ app.post('/tab', parseUrlencoded, function(request, response) {});
 
 app.delete('/tab', function(request, response) {});
 
-app.get('/search', function(request, response) {	
-	var requestQuery = request.query;
-	requestName			 = requestQuery.name;
-	beerQuery	       = requestName.split(' ').join('+');
-	beerChoice(response);
-});
-
-app.get('/queries', function(request, response) {
-	response.status(200).json();
-});
 
 var beers  = [
 	{name: "Light", abv: 4, img: "images/bottle.png", ounces: 12},
@@ -85,32 +80,6 @@ var outcomes = [
 	{name: "stage-nine", description: "You have little comprehension of where you are. You may pass out suddenly and be difficult to awaken. Coma is possible. This is the level of surgical anesthesia", color: "five", id: 9},
 	{name: "stage-ten", description: "Onset of coma, probable death due to respitory arrest", color: "five", id: 10}
 ];
-
-var beerChoice = function(responseObject) {
-	var	beersList = [];
-	target = 'http://api.brewerydb.com/v2/search?q=' + beerQuery + '&key=' + process.env.BREWERY_DB_KEY;
-	console.log('target', target);
-	request(target, function(err, response, body) {
-		if(!err && response.statusCode === 200) {
-			var beerResults = (JSON.parse(body));
-			var beerObjects = beerResults.data;			
-			beerObjects.forEach(function(potentialBeers) {
-				var beersOnly = {};
-				if (potentialBeers.type === 'beer') {
-			  	beersOnly.name  = potentialBeers.name;
-			  	beersOnly.abv	  = potentialBeers.abv;
-				 	beersList.push(beersOnly);
-					theBeer = beersList[0];
-				} 	
-			});
-			responseObject.json(theBeer);
-		} else {
-			console.log
-		}
-		console.log('beersList', beersList);	
-	});	
-	return beersList;
-};
 
 app.listen(3000, function() {
 	console.log('Server running on localhost:3000 \n');
